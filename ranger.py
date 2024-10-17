1bd49535a9f3ec77784294fc5a6d24dabe3afe5d
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-10-17 12:52:11 +0300

    Add files via upload

---
+++ ranger.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title='Box plots', page_icon=':chart_with_upwards_trend:')
+st.title('Box plots')
+uploaded_file= st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    column = st.selectbox('Select a column', df.columns.tolist())
+    title = st.text_input('Title', 'Box Plot')
+    x_label = st.text_input('X-axis Label', 'X-axis')
+    y_label = st.text_input('Y-axis Label', 'Y-axis')
+    fig= px.box(df, x=df.index, y=df[column])
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+    st.title("Descriptive summary")
+    st.write(df.describe())
