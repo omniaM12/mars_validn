6f2f23f633a9ce75379a0386adaa5d7f58d7576d
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-10-19 20:14:13 +0300

    Update ranger.streamlit_app.py

--- ranger.streamlit_app.py
+++ ranger.streamlit_app.py
@@ -11,33 +11,43 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     column = st.selectbox('Select a column', df.columns.tolist())
     title = st.selectbox('Select chart',('Box Plot','Bar plot'))
+    minr= st.number_input("Insert a number", value=None, placeholder="Type a number...")
+    maxr= st.number_input("Insert a number", value=None, placeholder="Type a number...")
     if title=='Box Plot':
         fig= px.box(df, x=df.index, y=df[column])
         st.plotly_chart(fig, theme=None, use_container_width=True)
-        st.title("Data columns type")
-        st.write(df.dtypes)
-        st.title("Missings")
-        st.write(df.isna().sum())
-        st.title("Descriptive summary")
-        st.write(df.describe())
+    st.title("Data columns type")
+    st.write(df.dtypes)
+    st.title("Missings")
+    st.write(df.isna().sum())
+    percent_missing = df.isnull().sum() * 100 / len(df)
+    if percent_missing>30:
+        columns_with_high_missingness = percent_missing[percent_missing > 30].index.tolist()
+        st.write("Columns with more than 30% missing values:", columns_with_high_missingness)
+        st.write("please check the columns with missings",columns_with_high_missingness)
+    st.title("Descriptive summary")
+    st.write(df.describe())
         
     if title=='Bar plot':
         fig= px.bar(df, x=df.index, y=df[column])
         st.plotly_chart(fig, theme=None, use_container_width=True)
     dfindex= df.select_dtypes(include=np.number)
-    min_index = dfindex.idxmin()
-    max_index = dfindex.idxmax()
+    filtered_df_less_than_minr = df[df[Column] < minr]
+    filtered_df_greater_than_maxr = df[df[Column1] > maxr]
 
-# Get the corresponding rows from the original DataFrame
-    min_row = df.loc[min_index]
-    max_row = df.loc[max_index]
+# Display the filtered DataFrames
+    if not filtered_df_less_than_minr.empty:
+        st.write("Rows less than min value:")
+        st.write(filtered_df_less_than_minr)
 
-# Display the results in Streamlit
-    st.write("Row with minimum values:")
-    st.write(min_row)
+    else:
+        st.write("No rows found less than min value.")
 
-    st.write("Row with maximum values:")
-    st.write(max_row)
+    if not filtered_df_greater_than_maxr.empty:
+        st.write("Rows greater than max value:")
+        st.write(filtered_df_greater_than_maxr)
+    else:
+        st.write("No rows found greater than max value.")
     
   
     
