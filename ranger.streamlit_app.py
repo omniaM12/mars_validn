d8644a131ca09b09973d7df20db29c414c92cf38
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-10-19 21:37:48 +0300

    Update ranger.streamlit_app.py

--- ranger.streamlit_app.py
+++ ranger.streamlit_app.py
@@ -39,8 +39,8 @@ if uploaded_file is not None:
         st.plotly_chart(fig, theme=None, use_container_width=True)
     dfindex= df.select_dtypes(include=np.number)
     if dfindex[column].isnumeric():
-        filtered_df_less_than_minr = dfindex[dfindex[column] <= minr]
-        filtered_df_greater_than_maxr = dfindex[dfindex[column] >= maxr]
+        filtered_df_less_than_minr = df[df[column] <= minr]
+        filtered_df_greater_than_maxr = df[df[column] >= maxr]
     else:
         st.write("the bar plot showing the available types in the data")
 # Display the filtered DataFrames
