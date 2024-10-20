f7940d9b0f735644a9c1d85c4eec2274ff7c7845
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-10-20 12:37:57 +0300

    Update ranger.streamlit_app.py

--- ranger.streamlit_app.py
+++ ranger.streamlit_app.py
@@ -37,17 +37,16 @@ if uploaded_file is not None:
         if title=='Box Plot':
             fig= px.box(df, y=df[column])
             st.plotly_chart(fig, theme=None, use_container_width=True)
-    
-            else:
-                fig= px.box(df, x=df.index, y=df[column])
-                st.plotly_chart(fig, theme=None, use_container_width=True)
+        else:
+            fig= px.box(df, x=df.index, y=df[column])
+            st.plotly_chart(fig, theme=None, use_container_width=True)
             
         if title=='Bar plot':
-            fig= px.bar(df, y=df[column])
+            fig= px.bar(df, x=df[column])
             st.plotly_chart(fig, theme=None, use_container_width=True)
-            else:
-                fig= px.box(df, x=df.index, y=df[column])
-                st.plotly_chart(fig, theme=None, use_container_width=True)
+        else:
+            fig= px.box(df, x=df.index, y=df[column])
+            st.plotly_chart(fig, theme=None, use_container_width=True)
         filtered_df_less_than_minr = df[df[column] <= minr]
         filtered_df_greater_than_maxr = df[df[column] >= maxr]
     # Display the filtered DataFrames
