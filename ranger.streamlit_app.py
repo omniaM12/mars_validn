6c03a0462b36ac313c2e1a68cca76eccd62cd9e5
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-10-20 12:53:56 +0300

    Update ranger.streamlit_app.py

--- ranger.streamlit_app.py
+++ ranger.streamlit_app.py
@@ -45,7 +45,7 @@ if uploaded_file is not None:
             fig= px.bar(df, x=df.index,y=df[column])
             st.plotly_chart(fig, theme=None, use_container_width=True)
         else:
-            fig= px.box(df,  y=df[column])
+            fig= px.bar(df,  y=df[column])
             st.plotly_chart(fig, theme=None, use_container_width=True)
         filtered_df_less_than_minr = df[df[column] <= minr]
         filtered_df_greater_than_maxr = df[df[column] >= maxr]
